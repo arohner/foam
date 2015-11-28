6180836113f7ae25a9d0433debf25a217b77d3a4
Allen Rohner <arohner@gmail.com>
2015-11-28 16:28:19 -0600

    More tests

--- test/foam/html_test.clj
+++ test/foam/html_test.clj
@@ -1,6 +1,7 @@
 (ns foam.html-test
   (:require [clojure.test :refer :all]
             [foam.core :as foam]
+            [foam.dom :as dom]
             [foam.html :as html]))
 
 (deftest parsing-tags
@@ -40,3 +41,29 @@
              [:h1 "Hello World"])]
     (is (satisfies? foam/ReactDOMRender ret))
     (is (-> ret :tag (= "h1")))))
+
+(deftest content-are-text-nodes
+  (let [ret (html/html
+             [:h1 "Hello World"])]
+    (is (satisfies? foam/ReactDOMRender ret))
+    (is (->> ret :children first (#(satisfies? foam/ReactDOMRender %))))))
+
+(defn simple-component [app owner opts]
+  (reify
+    foam/IRender
+    (render [this]
+      (dom/h1 nil "Hello World"))))
+
+(defn nested-component [app owner opts]
+  (reify
+    foam/IRender
+    (render [this]
+      (html/html
+       [:div
+        (foam/build simple-component app {})]))))
+
+(deftest handles-om-components
+  (let [state (atom {})
+        cursor (foam/root-cursor state)
+        com (foam/build nested-component cursor {})]
+    (is (string? (dom/render-to-string com)))))
