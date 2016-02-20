c2f23d5a917eb9d43a6d80cb53d7dd9b1703ff9a
Allen Rohner <arohner@gmail.com>
2016-02-20 16:44:27 -0600

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arohner/foam "0.1.6"
+(defproject arohner/foam "0.1.7-SNAPSHOT"
   :description "server-side rendering for Om"
   :url "http://github.com/arohner/foam"
   :license {:name "Eclipse Public License"
